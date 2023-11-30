005b7db902b85fa21a3891ed7e53c432280e26d1
inot65 <tft_toni@hotmail.com>
2023-11-30 22:16:54 +0200

    revenire

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const express = require('express');
-
 const io = require('socket.io')(8900, {
   // cors: {
   //   origin: 'https://client-chat-tn8z.onrender.com',
@@ -8,9 +6,6 @@ const io = require('socket.io')(8900, {
     origin: '*',
   },
 });
-
-// fac aplicatia principala
-const app = express();
 
 // fac o matrice a utilizatorilor conectati
 let users = [];
@@ -30,42 +25,32 @@ const getUser = (userId) => {
   return users.find((user) => user.userId === userId);
 };
 
-app.listen(8800, () => {
-  console.log('Backend server is running on 8800 port...');
+// lansare server socket
+io.on('connection', (socket) => {
+  console.log('S-a conectat un utilizator.');
+  // on connect
+  // iau userId si SocketId de la utilizator
+  // si il introduc in matrice, DOAR DACA nu exista deja
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id);
+    io.emit('getUsers', users);
+  });
 
-  // lansare server socket
-
-  io.on('connection', (socket) => {
-    console.log('S-a conectat un utilizator.');
-    // on connect
-    // iau userId si SocketId de la utilizator
-    // si il introduc in matrice, DOAR DACA nu exista deja
-    socket.on('addUser', (userId) => {
-      addUser(userId, socket.id);
-      io.emit('getUsers', users);
-    });
-
-    // send and get message
-    socket.on('sendMessage', ({senderId, receiverId, text}) => {
-      const user = getUser(receiverId);
-      // trimite la receiverId mesajul
-      io.to(user.socketId).emit('getMessage', {
-        senderId,
-        text,
-      });
-    });
-
-    // on disconnect
-    socket.on('disconnect', () => {
-      removeUser(socket.id);
-      console.log('Un user s-a deconectat de la serverul socket!');
-      // retrimit lista actualizata
-      io.emit('getUsers', users);
+  // send and get message
+  socket.on('sendMessage', ({senderId, receiverId, text}) => {
+    const user = getUser(receiverId);
+    // trimite la receiverId mesajul
+    io.to(user.socketId).emit('getMessage', {
+      senderId,
+      text,
     });
   });
 
-  // raspund la get pentru a putea da click automat
-  app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳 ');
+  // on disconnect
+  socket.on('disconnect', () => {
+    removeUser(socket.id);
+    console.log('Un user s-a deconectat de la serverul socket!');
+    // retrimit lista actualizata
+    io.emit('getUsers', users);
   });
 });
