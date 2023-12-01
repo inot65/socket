739eb9085f9e65ad799525f07bc643f9a0a3f814
inot65 <tft_toni@hotmail.com>
2023-12-01 18:47:43 +0200

    revin la forma veche pentru serverul Socket.IO

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-// const io = require('socket.io')(8900, {
-//   cors: {
-//     origin:
-//       process.env.NODE_ENV === 'production'
-//         ? 'https://client-chat-tn8z.onrender.com'
-//         : 'http://localhost:3000/',
-//   },
-// });
-
-const express = require('express');
-const {createServer} = require('node:https');
-const {join} = require('node:path');
-const {Server} = require('socket.io');
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {},
+const io = require('socket.io')(8900, {
   cors: {
     origin:
       process.env.NODE_ENV === 'production'
         ? 'https://client-chat-tn8z.onrender.com'
         : 'http://localhost:3000/',
   },
-});
-
-app.get('/', (req, res) => {
-  res.send('<h1>Server Socket.IO pt aplicatia de chat <b>-demo-</b></h1>');
 });
 
 // fac o matrice a utilizatorilor conectati
@@ -73,11 +52,4 @@ io.on('connection', (socket) => {
     // retrimit lista actualizata
     io.emit('getUsers', users);
   });
-});
-
-// ascult pe portul
-const port = 3000;
-
-server.listen(port, () => {
-  console.log('server running at port ', port);
 });
