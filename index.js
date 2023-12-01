060455f2d90651a70161a9b65ea8b148520c5258
inot65 <tft_toni@hotmail.com>
2023-12-01 18:06:34 +0200

    noua modalitate de constructie server IO

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const io = require('socket.io')(8900, {
-  cors: {
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? 'https://client-chat-tn8z.onrender.com'
-        : 'http://localhost:3000/',
-  },
+// const io = require('socket.io')(8900, {
+//   cors: {
+//     origin:
+//       process.env.NODE_ENV === 'production'
+//         ? 'https://client-chat-tn8z.onrender.com'
+//         : 'http://localhost:3000/',
+//   },
+// });
+
+const express = require('express');
+const {createServer} = require('node:http');
+const {join} = require('node:path');
+const {Server} = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Server Socket.IO pt aplicatia de chat <b>-demo-</b></h1>');
 });
 
 // fac o matrice a utilizatorilor conectati
@@ -52,4 +65,11 @@ io.on('connection', (socket) => {
     // retrimit lista actualizata
     io.emit('getUsers', users);
   });
+});
+
+// ascult pe portul
+const port = 3000;
+
+server.listen(port, () => {
+  console.log('server running at port ', port);
 });
